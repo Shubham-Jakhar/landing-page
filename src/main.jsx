ab6758fb564da8aa5d1e4a073f39eb85da584e2d
shubham jakhar <shubhamjakhar16@gmail.com>
2025-12-14 21:51:53 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import './app/globals.css';
+import './pages/globals.css';
 import { StrictMode } from 'react';
-import Home from './app/page';
+import Home from './pages/page';
 import ThemeProvider from '@/components/theme-provider'
 
 createRoot(document.getElementById('root')).render(
